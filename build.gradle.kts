e64cb8a0fb810cb5e3b87f96fb98b8f1850f43ae
Geir André Lund <geir.andre.lund@nav.no>
2020-04-27 20:14:55 +0200

    target and source compatibility to java 8

--- build.gradle.kts
+++ build.gradle.kts
@@ -44,8 +44,8 @@ subprojects {
     }
 
     java {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
     }
 
     tasks.named<KotlinCompile>("compileKotlin") {
