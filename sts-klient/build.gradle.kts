d6d28948c51b79273f542950e81e36c882863043
knut riise <knut.magne.riise@nav.no>
2021-02-02 13:08:39 +0100

    erstattet fuel med ktor-client
    Co-authored-by: Giao The Cung <giaothe@gmail.com>

--- sts-klient/build.gradle.kts
+++ sts-klient/build.gradle.kts
@@ -1,11 +1,11 @@
-val fuelVersion = "2.1.0"
 
 dependencies {
-    implementation(Fuel.fuel)
     implementation(Ktor.library("client-cio-jvm"))
     implementation(Ktor.library("client-jackson"))
+    implementation(Ktor.library("client-logging"))
     implementation(Ktor.library("client-auth-jvm"))
-    implementation(Fuel.library("gson"))
     implementation(Prometheus.common)
     testImplementation(Wiremock.standalone)
+    testImplementation("ch.qos.logback:logback-classic:1.2.3")
+    testImplementation( Ktor.library("client-mock"))
 }
--- sts-klient/src/main/kotlin/no/nav/dagpenger/oidc/StsOidcClient.kt
+++ sts-klient/src/main/kotlin/no/nav/dagpenger/oidc/StsOidcClient.kt
@@ -1,9 +1,21 @@
 package no.nav.dagpenger.oidc
 
 import io.ktor.client.HttpClient
+import io.ktor.client.engine.HttpClientEngine
 import io.ktor.client.engine.cio.CIO
 import io.ktor.client.features.auth.Auth
 import io.ktor.client.features.auth.providers.basic
+import io.ktor.client.features.json.JacksonSerializer
+import io.ktor.client.features.json.JsonFeature
+import io.ktor.client.features.logging.DEFAULT
+import io.ktor.client.features.logging.LogLevel
+import io.ktor.client.features.logging.Logger
+import io.ktor.client.features.logging.Logging
+import io.ktor.client.request.get
+import io.ktor.client.request.header
+import io.ktor.client.request.parameter
+import io.ktor.http.ContentType
+import io.ktor.http.contentType
 import io.ktor.util.KtorExperimentalAPI
 import io.prometheus.client.Summary
 import kotlinx.coroutines.Dispatchers
@@ -28,14 +40,24 @@ private val requestLatency = Summary.build()
 
 @KtorExperimentalAPI
 @ExperimentalTime
-class StsOidcClient(stsBaseUrl: String, private val username: String, private val password: String) : OidcClient {
+class StsOidcClient(stsBaseUrl: String, private val username: String, private val password: String, engine: HttpClientEngine = CIO.create()) : OidcClient {
     @KtorExperimentalAPI
-    private val client = HttpClient(CIO) {
+    private val client = HttpClient(engine) {
+        install(JsonFeature) {
+            serializer = JacksonSerializer {
+
+            }
+        }
         install(Auth) {
             basic {
+                sendWithoutRequest = true
                 username = this@StsOidcClient.username
                 password = this@StsOidcClient.password
             }
+        }
+        install(Logging){
+            logger = Logger.DEFAULT
+            level = LogLevel.ALL
         }
     }
 
@@ -58,6 +80,15 @@ class StsOidcClient(stsBaseUrl: String, private val username: String, private va
 
     private suspend fun newOidcToken(): OidcToken {
         return withContext(Dispatchers.IO) {
+            kotlin.runCatching {
+                client.get<OidcToken>(stsTokenUrl) {
+                    parameter("grant_type", "client_credentials")
+                    parameter("scope", "openid")
+                    contentType(ContentType.Application.Json)
+                }
+            }.getOrElse {
+                throw StsOidcClientException(it.localizedMessage, it)
+            }
         }
     }
 }
@@ -79,7 +110,7 @@ data class OidcToken(
     companion object {
         fun isValid(token: OidcToken?) = when (token) {
             null -> false
-            else -> !token.valid
+            else -> token.valid
         }
     }
 }
