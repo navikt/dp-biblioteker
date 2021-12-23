ddc2581b23c5c4def07f693003d4b6de6b2b4574
Giao The Cung <giaothe@gmail.com>
2021-12-23 14:59:13 +0100

    Scaler

---
+++ image-utils/src/main/kotlin/no/nav/dagpenger/pdf/ImageConverter.kt
@@ -0,0 +1,92 @@
+package no.nav.dagpenger.pdf
+
+import no.nav.dagpenger.pdf.Detect.isImage
+import no.nav.dagpenger.pdf.Detect.isPdf
+import no.nav.dagpenger.pdf.ImageConverter.requireImage
+import org.apache.pdfbox.pdmodel.PDDocument
+import org.apache.pdfbox.pdmodel.PDPage
+import org.apache.pdfbox.pdmodel.PDPageContentStream
+import org.apache.pdfbox.pdmodel.common.PDRectangle
+import org.apache.pdfbox.pdmodel.graphics.image.PDImageXObject
+import org.imgscalr.Scalr
+import java.awt.Dimension
+import java.awt.image.BufferedImage
+import java.io.ByteArrayOutputStream
+import java.io.InputStream
+import javax.imageio.ImageIO
+import kotlin.math.max
+import kotlin.math.min
+
+object ImageConverter {
+
+    internal fun requireImage(input: ByteArray) {
+        require(input.isImage()) { "Only jpg and png is supported" }
+    }
+
+    internal fun requireImage(input: InputStream) {
+        require(input.isImage()) { "Only jpg and png is supported" }
+    }
+
+    fun toPDF(input: ByteArray): ByteArray {
+        if (input.isPdf()) return input
+        requireImage(input)
+
+        return ByteArrayOutputStream().use { os ->
+            PDDocument().use { document ->
+                val image = PDImageXObject.createFromByteArray(document, input, null)
+                val page = PDPage(PDRectangle(image.width.toFloat(), image.height.toFloat()))
+                document.addPage(page)
+
+                PDPageContentStream(document, page).use {
+                    it.drawImage(image, 0F, 0F)
+                }
+                document.save(os)
+            }
+            os.toByteArray()
+        }
+    }
+}
+
+object ImageScaler {
+    enum class ScaleMode {
+        SCALE_TO_FIT_INSIDE_BOX,
+        CROP_TO_FILL_ENTIRE_BOX
+    }
+
+    fun scale(input: InputStream, dimension: Dimension, scaleMode: ScaleMode): BufferedImage {
+        requireImage(input)
+        return input.use { scale(ImageIO.read(input), dimension, scaleMode) }
+    }
+
+    fun scale(input: BufferedImage, dimension: Dimension, scaleMode: ScaleMode): BufferedImage {
+        val scaleFactorWidth: Double = dimension.getWidth() / input.width
+        val scaleFactorHeight: Double = dimension.getHeight() / input.height
+
+        val scalingFactor = when (scaleMode) {
+            ScaleMode.SCALE_TO_FIT_INSIDE_BOX -> min(scaleFactorWidth, scaleFactorHeight)
+            ScaleMode.CROP_TO_FILL_ENTIRE_BOX -> max(scaleFactorWidth, scaleFactorHeight)
+        }
+
+        val scaledImage = Scalr.resize(
+            input,
+            (scalingFactor * input.width).toInt(),
+            (scalingFactor * input.height).toInt()
+        )
+
+        return when (scaleMode) {
+            ScaleMode.SCALE_TO_FIT_INSIDE_BOX -> scaledImage
+            ScaleMode.CROP_TO_FILL_ENTIRE_BOX -> crop(scaledImage, dimension)
+        }
+    }
+
+    fun crop(image: BufferedImage, dimension: Dimension): BufferedImage {
+        require(image.width >= dimension.width && image.height >= dimension.height) {
+            "Image must be at least as big as dimension"
+        }
+
+        val widthDelta = image.width - dimension.width
+        val heightDelta = image.height - dimension.height
+
+        return image.getSubimage(widthDelta / 2, heightDelta / 2, dimension.width, dimension.height)
+    }
+}
--- image-utils/src/test/kotlin/no/nav/dagpenger/pdf/ImageConverterTest.kt
+++ image-utils/src/test/kotlin/no/nav/dagpenger/pdf/ImageConverterTest.kt
@@ -1,7 +1,9 @@
 package no.nav.dagpenger.pdf
 
+import io.kotest.assertions.throwables.shouldThrow
 import io.kotest.matchers.types.shouldBeSameInstanceAs
 import io.kotest.matchers.types.shouldBeTypeOf
+import org.junit.jupiter.api.Disabled
 import org.junit.jupiter.api.Test
 import java.io.BufferedOutputStream
 import java.io.FileOutputStream
@@ -11,35 +13,48 @@ internal class ImageConverterTest {
     @Test
     fun `konverter ikke pdf`() {
         "/pdfs/minimal.pdf".fileAsByteArray().let {
-            Image.toPDF(it) shouldBeSameInstanceAs it
+            ImageConverter.toPDF(it) shouldBeSameInstanceAs it
         }
     }
 
     @Test
     fun `konverter png til pdf`() {
         "/images/bilde.png".fileAsByteArray().let {
-            PDFDocument.load(Image.toPDF(it)).shouldBeTypeOf<ValidPDFDocument>()
+            PDFDocument.load(ImageConverter.toPDF(it)).use { document ->
+                document.shouldBeTypeOf<ValidPDFDocument>()
+            }
         }
     }
 
     @Test
-    fun `konverter jpg til pdf`() {
+    fun `konverter jpeg til pdf`() {
         "/images/bilde.jpeg".fileAsByteArray().let {
-            PDFDocument.load(Image.toPDF(it)).shouldBeTypeOf<ValidPDFDocument>()
+            PDFDocument.load(ImageConverter.toPDF(it)).use { document ->
+                document.shouldBeTypeOf<ValidPDFDocument>()
+            }
         }
     }
 
     @Test
-//    @Disabled("Manuell verifkasjon test som skriver til filsystem")
+    fun `kan bare konvertere jpeg eller png`() {
+        shouldThrow<IllegalArgumentException> { ImageConverter.toPDF("hbba".toByteArray()) }
+    }
+
+    @Test
+    @Disabled
     fun `konverter og skriv til filsystem`() {
         "/images/bilde.png".fileAsByteArray().let {
-            PDFDocument.load(Image.toPDF(it)).shouldBeTypeOf<ValidPDFDocument>().also { document ->
-                document.save(BufferedOutputStream(FileOutputStream("bilde_png.pdf")))
+            PDFDocument.load(ImageConverter.toPDF(it)).shouldBeTypeOf<ValidPDFDocument>().also { document ->
+                BufferedOutputStream(FileOutputStream("/build/tmp/bilde_png.pdf")).use { os ->
+                    document.save(os)
+                }
             }
         }
         "/images/bilde.jpeg".fileAsByteArray().let {
-            PDFDocument.load(Image.toPDF(it)).shouldBeTypeOf<ValidPDFDocument>().also { document ->
-                document.save(BufferedOutputStream(FileOutputStream("bilde_jepg.pdf")))
+            PDFDocument.load(ImageConverter.toPDF(it)).shouldBeTypeOf<ValidPDFDocument>().also { document ->
+                BufferedOutputStream(FileOutputStream("/build/tmp/bilde_jpeg.pdf")).use { os ->
+                    document.save(os)
+                }
             }
         }
     }
