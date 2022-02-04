db267ed637e9dc173a07108ac567d09562ccb5ce
Giao The Cung <giaothe@gmail.com>
2022-02-04 10:00:17 +0100

    Revert "Bump source and target jvm version"
    This reverts commit 032898cb97f30b9e62cd58f4916eb7c6266049ed.

--- build.gradle.kts
+++ build.gradle.kts
@@ -40,16 +40,16 @@ subprojects {
     }
 
     java {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
     }
 
     tasks.named<KotlinCompile>("compileKotlin") {
-        kotlinOptions.jvmTarget = "11"
+        kotlinOptions.jvmTarget = "1.8"
     }
 
     tasks.named<KotlinCompile>("compileTestKotlin") {
-        kotlinOptions.jvmTarget = "11"
+        kotlinOptions.jvmTarget = "1.8"
     }
 
     tasks.withType<Test> {
