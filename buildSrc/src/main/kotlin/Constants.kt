4f9161a85b7664cc5613c5f48c3974556497177e
Giao The Cung <giaothe@gmail.com>
2022-06-20 08:11:59 +0200

    Oppgradere Constants

--- buildSrc/src/main/kotlin/Constants.kt
+++ buildSrc/src/main/kotlin/Constants.kt
@@ -45,15 +45,15 @@ object Dagpenger {
     }
 
     const val Grunnbeløp = "com.github.navikt:dp-grunnbelop:2022.05.20-09.36.a361a7eb490e"
-    const val Streams = "com.github.navikt:dagpenger-streams:2021.12.29-13.08.7857ff52b457"
+    const val Streams = "com.github.navikt:dagpenger-streams:2022.06.19-20.52.d37841b711ec"
     const val Events = "com.github.navikt:dagpenger-events:2021.12.14-09.45.e24e8c813b27"
 }
 
 object Database {
-    const val Postgres = "org.postgresql:postgresql:42.2.22"
+    const val Postgres = "org.postgresql:postgresql:42.3.2"
     const val Kotlinquery = "com.github.seratch:kotliquery:1.6.1"
-    const val Flyway = "org.flywaydb:flyway-core:7.10.0"
-    const val HikariCP = "com.zaxxer:HikariCP:4.0.3"
+    const val Flyway = "org.flywaydb:flyway-core:8.5.11"
+    const val HikariCP = "com.zaxxer:HikariCP:5.0.1"
     const val VaultJdbc = "no.nav:vault-jdbc:1.3.7"
 }
 
@@ -118,7 +118,7 @@ object Konfig {
 }
 
 object Kotlin {
-    const val version = "1.6.20"
+    const val version = "1.7.0"
     const val stdlib = "org.jetbrains.kotlin:kotlin-stdlib:$version"
 
     object Coroutines {
@@ -175,7 +175,7 @@ object Ktor2 {
 }
 
 object Log4j2 {
-    const val version = "2.17.0"
+    const val version = "2.17.1"
     const val api = "org.apache.logging.log4j:log4j-api:$version"
     const val core = "org.apache.logging.log4j:log4j-core:$version"
     const val slf4j = "org.apache.logging.log4j:log4j-slf4j-impl:$version"
@@ -228,6 +228,7 @@ object Prometheus {
 }
 
 const val RapidAndRivers = "com.github.navikt:rapids-and-rivers:2022.03.25-09.17.0628678a7192"
+const val RapidAndRiversKtor2 = "com.github.navikt:rapids-and-rivers:2022060808531654671206.908d671b7ae0"
 
 object Slf4j {
     const val version = "1.7.25"
@@ -249,7 +250,7 @@ object Shadow {
 }
 
 object TestContainers {
-    const val version = "1.16.2"
+    const val version = "1.17.1"
     const val postgresql = "org.testcontainers:postgresql:$version"
     const val kafka = "org.testcontainers:kafka:$version"
 }
