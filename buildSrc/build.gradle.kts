d531873ed61cf579973f6a2cc43a64814472fbcc
Giao The Cung <giaothe@gmail.com>
2023-10-30 06:09:51 +0100

    Oppgrade spotless

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
     `kotlin-dsl`
-    id("com.diffplug.spotless") version "6.19.0"
+    id("com.diffplug.spotless") version "6.22.0"
 }
 
 repositories {
@@ -10,10 +10,8 @@ repositories {
 
 dependencies {
     implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.0")
-    implementation("com.diffplug.spotless:spotless-plugin-gradle:6.19.0")
+    implementation("com.diffplug.spotless:spotless-plugin-gradle:6.22.0")
 }
-
-
 
 spotless {
     kotlinGradle {
