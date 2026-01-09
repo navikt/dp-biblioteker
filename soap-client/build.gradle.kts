7a209558d37e0842e39910806028a524459d0e31
Igor Weber <igor.weber@nav.no>
2026-01-09 15:49:49 +0100

    Oppdatert soap-client

--- soap-client/build.gradle.kts
+++ soap-client/build.gradle.kts
@@ -6,20 +6,20 @@ val tjenestespesifikasjonerVersion = "2643.2f3e8e9"
 
 fun tjenestespesifikasjon(name: String) = "com.github.navikt.tjenestespesifikasjoner:$name:$tjenestespesifikasjonerVersion"
 
-val cxfVersion = "3.6.6"
+val cxfVersion = "4.1.4"
 
 dependencies {
     implementation("de.huxhorn.sulky:de.huxhorn.sulky.ulid:8.3.0")
 
-    implementation("javax.xml.ws:jaxws-api:2.3.1")
-    implementation("com.sun.xml.ws:jaxws-tools:2.3.7")
+    implementation("jakarta.xml.ws:jakarta.xml.ws-api:4.0.2")
+    implementation("com.sun.xml.ws:jaxws-tools:4.0.3")
 
     implementation("org.apache.cxf:cxf-rt-features-logging:$cxfVersion")
     implementation("org.apache.cxf:cxf-rt-frontend-jaxws:$cxfVersion")
     implementation("org.apache.cxf:cxf-rt-ws-policy:$cxfVersion")
     api("org.apache.cxf:cxf-rt-ws-security:$cxfVersion")
     implementation("org.apache.cxf:cxf-rt-transports-http:$cxfVersion")
-    implementation("javax.activation:activation:1.1.1")
+    implementation("jakarta.activation:jakarta.activation-api:2.1.4")
 
     testImplementation("org.apache.cxf:cxf-rt-transports-http:$cxfVersion")
     testImplementation(tjenestespesifikasjon("ytelseskontrakt-v3-tjenestespesifikasjon"))
